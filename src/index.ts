aa679944faefc2784e75c08eac3d80166ec07bfd
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-03-14 23:35:39 -0600

    Add Discord client stack

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
 import "source-map-support/register";
-console.log("Hello, world!");
+import Discord from "discord.js";
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user?.tag ?? "nobody right now"}!`);
+});
+
+client.on("message", msg => {
+  if (msg.content === "!ping") {
+    void msg.reply("Pong!");
+  }
+});
+
+void client.login(process.env.DISCORD_TOKEN);
