4834734df93333249aa87e4e74c1e51235afb4d4
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-03-16 10:03:52 -0600

    Add a console log with the ping result

--- src/commands/pingResponse.ts
+++ src/commands/pingResponse.ts
@@ -4,12 +4,15 @@ import { randomPhrase } from "../actions/randomStrings";
 export default async function pong(context: CommandContext): Promise<string> {
   const { client, message } = context;
 
+  const apiLatency = Math.round(client.ws.ping);
+
   const testMessage = await message.channel.send(randomPhrase());
-  await testMessage.edit(
-    `Pong! Sent response in \`${
-      testMessage.createdTimestamp - message.createdTimestamp
-    }ms\`. API Latency is \`${Math.round(client.ws.ping)}ms\``
+  const responseTime = testMessage.createdTimestamp - message.createdTimestamp;
+
+  const response = await testMessage.edit(
+    `Pong! Sent response in \`${responseTime}ms\`. API Latency is \`${apiLatency}ms\``
   );
+  console.log(`Out-of-band response: '${response.content}'`);
 
   return "";
 }
