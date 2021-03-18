94aaad1453ccb0d2a7bfa23c9940694b20520095
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-03-18 00:25:42 -0600

    Add reaction support

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,9 @@ try {
 
   client.on("ready", () => {
     logger.info(`Logged in as ${client.user?.tag ?? "nobody right now"}!`);
+
+    // TODO: Set own status to Active
+    // TODO: On exit in prod mode, set own status to Offline
   });
 
   client.on("message", msg => {
@@ -20,6 +23,32 @@ try {
       .catch(error =>
         logger.error(`Failed to handle command: ${JSON.stringify(error, undefined, 2)}`)
       );
+  });
+
+  client.on("messageReactionAdd", (reaction, user) => {
+    if (reaction.me) return; // Ignore self reaction
+
+    // TODO: If reaction is :x: and the message is a queue message, mark that queue entry as "ignored"
+    logger.info(
+      `User ${user.id} (${user.username ?? "null"}) reacted with :${
+        reaction.emoji.identifier
+      }: on message ${reaction.message.id} in channel ${reaction.message.channel.id} in guild ${
+        reaction.message.guild?.id ?? "(null)"
+      }`
+    );
+  });
+
+  client.on("messageReactionRemove", (reaction, user) => {
+    if (reaction.me) return; // Ignore self reaction
+
+    // TODO: If reaction is :x: and the message is a queue message, unignore that queue entry
+    logger.info(
+      `User ${user.id} (${user.username ?? "null"}) removed a reaction on message ${
+        reaction.message.id
+      } in channel ${reaction.message.channel.id} in guild ${
+        reaction.message.guild?.id ?? "(null)"
+      }`
+    );
   });
 
   void client.login(process.env.DISCORD_TOKEN);
