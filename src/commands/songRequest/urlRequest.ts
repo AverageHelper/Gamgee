22592352d6616af82066c194ef5db729fe93a3b3
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-03-23 21:34:24 -0600

    Don't be so cheeky with request acceptance

--- src/commands/songRequest/urlRequest.ts
+++ src/commands/songRequest/urlRequest.ts
@@ -3,7 +3,6 @@ import { MILLISECONDS_IN_SECOND } from "../../constants/time";
 import { useLogger } from "../../logger";
 import { useQueue, UnsentQueueEntry } from "../../actions/queue/useQueue";
 import { reject_public, reject_private } from "./index";
-import { randomAcceptance } from "../../helpers/randomStrings";
 import getQueueChannel from "../../actions/queue/getQueueChannel";
 import getVideoDetails from "../../actions/getVideoDetails";
 import durationString from "../../helpers/durationString";
@@ -58,9 +57,7 @@ const urlRequest: ArbitrarySubcommand = {
         `Pushed new entry to queue. Sending public acceptance to user ${message.author.id} (${message.author.username})`
       );
       // Send acceptance after the potential `send(entry.url)` call
-      await message.channel.send(
-        `<@!${message.author.id}>, ${randomAcceptance()}\n\nSubmission Accepted!`
-      );
+      await message.channel.send(`<@!${message.author.id}>, Submission Accepted!`);
       logger.debug("Responded.");
     }
 
