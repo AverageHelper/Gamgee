edfaed5a82d7dc1e9583a113ff1cd97a19564541
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-03-24 12:31:48 -0600

    Add an emote to DMed rejections

--- src/commands/songRequest/index.ts
+++ src/commands/songRequest/index.ts
@@ -27,7 +27,7 @@ export async function reply_private(message: Discord.Message, msg: string): Prom
 export async function reject_private(message: Discord.Message, reason: string): Promise<void> {
   await Promise.all([
     deleteMessage(message, "Spam; this song request was rejected."),
-    replyPrivately(message, reason)
+    replyPrivately(message, `:hammer: ${reason}`)
   ]);
 }
 
