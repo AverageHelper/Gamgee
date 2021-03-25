2d8480b4654ff2d04dd0ef3ff9773f19bc8053fa
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-03-24 20:07:25 -0600

    Abstract out the test bot's wait loop factories

---
+++ tests/discordUtils/waitLoop.ts
@@ -0,0 +1,34 @@
+import type Discord from "discord.js";
+import { useTestLogger } from "../testUtils/logger";
+
+const logger = useTestLogger();
+
+/**
+ * Creates a function that manages a collection of waiting functions.
+ * New events are sent through these waiter functions, and are removed
+ * as waiters handle them.
+ *
+ * @param waiterCollection A collection of waiter functions. Each function
+ * either handles the event and returns `true`, or doesn't handle the event
+ * and returns `false`.
+ *
+ * @returns an event receiver function.
+ */
+export function useWaitLoop<T>(
+  waiterCollection: Discord.Collection<number, (arg: T) => boolean>
+): (arg: T) => void {
+  return function (arg) {
+    if (waiterCollection.size === 0) return;
+
+    const removed = waiterCollection.sweep((waiter, id) => {
+      const shouldRemove = waiter(arg);
+      if (shouldRemove) {
+        logger.debug(`Waiter ${id} handled a deleted message. Removing from the loop...`);
+      } else {
+        logger.debug(`Waiter ${id} did not handle the message. Keeping in the loop.`);
+      }
+      return shouldRemove;
+    });
+    logger.debug(`Removed ${removed} finished waiters.`);
+  };
+}
