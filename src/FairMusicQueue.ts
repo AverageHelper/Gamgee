4f61be5cd135477e5ce15a6b0e503bc301be9d43
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-03-25 22:11:48 -0600

    More culling of unnecessary constructs; use our naming scheme in more places.

--- src/FairMusicQueue.ts
+++ src/FairMusicQueue.ts
@@ -3,28 +3,16 @@
  * Node.js port by AverageHelper
  */
 
-import { useLogger } from "./logger";
-const logger = useLogger();
-
-/** Return the first element of an array, or `null` if the array is empty. */
-function peek<T>(linkedList: Array<T>): T | null {
-  if (linkedList.length === 0) {
-    return null;
-  }
-  return linkedList[0];
-}
+export {};
 
 /** Return the last element of an array, or `null` if the array is empty. */
-function peekLast<T>(linkedList: Array<T>): T | null {
-  if (linkedList.length === 0) {
-    return null;
-  }
-  return linkedList[linkedList.length - 1];
+function peekLast<T>(linkedList: ReadonlyArray<T> | Array<T>): T | null {
+  return linkedList[linkedList.length - 1] ?? null;
 }
 
 // Container object for submissions
 interface Submission {
-  link: string;
+  url: string;
   secondsLong: number;
   timeCreated: number;
 }
@@ -34,23 +22,21 @@ interface Submission {
 // This way latecomers get to join the party as soon as possible while those
 // that showed up early still get to hear at least their first submission
 // before getting shifted back in the line.
-class FairerQueue {
-  #queue: Array<User> = [];
+class PriorityQueue {
+  #storage: Array<User> = [];
 
-  // synchronized so we can make sure we're not double-adding a user in
-  // case of spam/lag spikes.
   add(user: User): void {
     // Only add user to the queue if they're not already in it.
-    if (!this.#queue.includes(user)) {
+    if (!this.#storage.includes(user)) {
       if (user.history.length !== 0) {
         // User has played a song before, no special treatment so
         // add them straight to the back of the queue.
-        this.#queue.push(user);
+        this.#storage.push(user);
       } else {
         // User has not played a song before, put them in line
         // before users that have already played a song.
         let insertPosition = 0;
-        for (const nextUser of this.#queue) {
+        for (const nextUser of this.#storage) {
           if (nextUser.history.length !== 0) {
             // Found a user that has already played a song,
             // break the loop, insertPosition = that user's position.
@@ -58,38 +44,46 @@ class FairerQueue {
           }
           insertPosition++;
         }
-        this.#queue.splice(insertPosition, 0, user);
+        this.#storage.splice(insertPosition, 0, user);
       }
     }
   }
 
   isEmpty(): boolean {
-    return this.#queue.length === 0;
+    return this.#storage.length === 0;
   }
 
   peek(): User | null {
-    return peek(this.#queue);
+    return this.#storage[0] ?? null;
   }
 
-  poll(): User | null {
-    return this.#queue.pop() ?? null;
+  pop(): User | null {
+    return this.#storage.pop() ?? null;
   }
 
   [Symbol.iterator]() {
-    return this.#queue.values();
+    return this.#storage.values();
   }
 }
 
 // Container object for Users (discord userId + local queue and history)
 class User {
   readonly userId: number;
-  localQueue: Array<Submission>;
-  history: Array<Submission>;
+  #localQueue: Array<Submission>;
+  #history: Array<Submission>;
 
   constructor(userId: number) {
     this.userId = userId;
-    this.localQueue = [];
-    this.history = [];
+    this.#localQueue = [];
+    this.#history = [];
+  }
+
+  get localQueue(): ReadonlyArray<Submission> {
+    return this.#localQueue;
+  }
+
+  get history(): ReadonlyArray<Submission> {
+    return this.#history;
   }
 
   hasSubmission(): boolean {
@@ -108,128 +102,112 @@ class User {
 
   addSubmission(sub: Submission): void {
     this.cleanOld();
-    this.localQueue.push(sub);
+    this.#localQueue.push(sub);
   }
 
   // Get next submission (if one exists), remove it from local queue and
   // add to history queue/list
   pollSubmission(): Submission | null {
     this.cleanOld();
-    const next = this.localQueue.pop() ?? null;
+    const next = this.#localQueue.pop() ?? null;
     if (next !== null) {
-      this.history.push(next);
+      this.#history.push(next);
     }
     return next;
   }
 
   // Clear out submissions that are too old/free up memory.
   cleanOld(): void {
-    const oldestAllowed = Date.now() - FairMusicQueueTest.maxSubmissionCacheTime;
+    const oldestAllowed = Date.now() - Queue.maxSubmissionCacheTime;
     while (
       this.history.length !== 0 &&
-      (peek(this.history)?.timeCreated ?? Number.MIN_VALUE) < oldestAllowed
+      (this.history[0]?.timeCreated ?? Number.MIN_VALUE) < oldestAllowed
     ) {
-      this.history.pop();
+      this.#history.pop();
     }
   }
 }
 
-class FairMusicQueueTest {
+class Queue {
   // Queues users rather than songs. Each user keeps its own local song queue,
   // and is added to the end of the master queue again after each of their
   // songs is played, until they have no songs left.
-  masterQueue: FairerQueue;
+  #masterQueue: PriorityQueue = new PriorityQueue();
 
   // Discord's userId -> User object
   // Filled with fake userIds and dummy User objects for testing but this
   // would be the persistent user/record storage
-  userMap: Record<number, User | undefined>;
+  #userMap: Record<number, User | undefined> = {};
 
   // Old/already-played user submissions will be forgotten over time, this
   // defines how long that takes.
   // Possibly, users that no longer have any current/historical
   // submissions can themselves be forgotten (not implemented in this test).
   // millis/sec * sec/min * 300 minutes
-  static maxSubmissionCacheTime: number = 1000 * 60 * 300;
+  static readonly maxSubmissionCacheTime: number = 1000 * 60 * 300;
 
   // Cooldown between submissions by the same user, in milliseconds.
   // Set to 0 for testing, can be higher to prevent spam.
-  recentSubmissionCooldown = 1000 * 60 * 0;
+  readonly recentSubmissionCooldown: number = 1000 * 60 * 0;
 
   // Maximum queued submissions per person
   // - once this limit is reached, the user's first song must be played
   // before they can submit again
   // - could probably set this higher given the fairer queueing system
-  maxPersonalSubmissions = 3;
+  readonly maxPersonalSubmissions: number = 3;
 
   // Maximum song length, stored in seconds
-  maxSubmissionLength = 60 * 10;
+  readonly maxSubmissionLength: number = 60 * 10;
 
-  constructor() {
-    this.masterQueue = new FairerQueue();
-    this.userMap = {};
-  }
-
-  // returns true if the song was submitted, false otherwise
-  // could be made to return some kind of String result or something
-  // rather than printing out some debug
-  trySubmission(userId: number, link: string, seconds: number): boolean {
+  /**
+   * Attempts to submit a song to the queue. Returns `false` if the submission
+   * was denied for any reason.
+   *
+   * @param userId The ID of the submitting user.
+   * @param url The URL to their submission.
+   * @param seconds The duration of their submission
+   * @returns `true` if the submission was accepted. `false` otherwise.
+   */
+  trySubmission(userId: number, url: string, seconds: number): boolean {
     if (seconds > this.maxSubmissionLength) {
-      logger.info(
-        `Submissioni denied, too long: User: ${userId} - Link: ${link} - Time: ${(
-          seconds / 60
-        ).toFixed(2)}:${(seconds % 60).toFixed(2)}`
-      );
+      // Submission denied. Song too long.
       return false;
     }
     // Get an existing User or create a new one if one doesn't exist.
-    const user: User = this.userMap[userId] ?? new User(userId);
+    const user: User = this.#userMap[userId] ?? new User(userId);
     const recent = user.getMostRecentSubmission();
     if (recent !== null && Date.now() - recent.timeCreated < this.recentSubmissionCooldown) {
-      logger.info(
-        `Submissioni denied, too recent: User: ${userId} - Link: ${link} - Time: ${(
-          seconds / 60
-        ).toFixed(2)}:${(seconds % 60).toFixed(2)}`
-      );
-      return false; // Could not submit, too recent
+      // Submission denied. User is still cooling down.
+      return false;
     }
     if (user.localQueue.length >= this.maxPersonalSubmissions) {
-      logger.info(
-        `Submission denied, too many submissions by user: User: ${userId} - Link: ${link} - Time: ${(
-          seconds / 60
-        ).toFixed(2)}:${(seconds % 60).toFixed(2)}`
-      );
-      return false; // Could not submit, too many submissions
+      // Submission denied. Too many submissions from this user.
+      return false;
     }
-    const submission: Submission = { link, secondsLong: seconds, timeCreated: Date.now() };
+    const submission: Submission = { url, secondsLong: seconds, timeCreated: Date.now() };
     // Add submission to user's local queue, also cleaning user's history
     // cache. This should be run before adding to the master queue.
     user.addSubmission(submission);
-    // Add user to master queue if not already in it (FairerQueue handles
+    // Add user to master queue if not already in it (PriorityQueue handles
     // that limitation itself)
-    this.masterQueue.add(user);
+    this.#masterQueue.add(user);
 
-    logger.info(
-      `Submission added! User: ${userId} - Link: ${submission.link} - Time: ${(
-        submission.secondsLong / 60
-      ).toFixed(2)}:${(submission.secondsLong % 60).toFixed(2)}`
-    );
-
+    // Submission accepted!
     return true;
   }
 
   pollNextSubmission(): Submission | null {
     // Loop until there's no more users in the queue (so no more submissions)
     // OR until a submission is found and returned.
-    while (!this.masterQueue.isEmpty()) {
-      const user = this.masterQueue.poll();
+    while (!this.#masterQueue.isEmpty()) {
+      const user = this.#masterQueue.pop();
 
       // Get next submission from user
       const nextSubmission = user?.pollSubmission() ?? null;
       if (nextSubmission !== null) {
         // Add user back to master queue if they have more submissions
         if (user?.hasSubmission()) {
-          this.masterQueue.add(user);
+          this.#masterQueue.add(user);
         }
         return nextSubmission;
       }
@@ -239,7 +217,7 @@ class FairMusicQueueTest {
 
   getTotalSubmissionTime(): number {
     let total = 0;
-    for (const user of this.masterQueue) {
+    for (const user of this.#masterQueue) {
       for (const sub of user.localQueue) {
         total += sub.secondsLong;
       }
@@ -250,69 +228,69 @@ class FairMusicQueueTest {
 
 //////// Just A Buncha Test Code ////////
 
-const queueManager = new FairMusicQueueTest();
+// const queueManager = new Queue();
 
-// Generate 4-8 users
-const numberOfTestUsers = Math.floor(Math.random() * 5 + 4);
+// // Generate 4-8 users
+// const numberOfTestUsers = Math.floor(Math.random() * 5 + 4);
 
-let submissionNumber = 0; // just to generate "unique" """submissions"""
-// More convenient to start at a high number and work towards zero for
-// testing fake user ids, so for this test, higher user IDs will try to
-// queue their songs first.
-for (let uNum = 0; uNum < numberOfTestUsers; uNum++) {
-  // create fake discord user id, easy to recognize in the test output
-  const userId = uNum * 1111;
+// let submissionNumber = 0; // just to generate "unique" """submissions"""
+// // More convenient to start at a high number and work towards zero for
+// // testing fake user ids, so for this test, higher user IDs will try to
+// // queue their songs first.
+// for (let uNum = 0; uNum < numberOfTestUsers; uNum++) {
+//   // create fake discord user id, easy to recognize in the test output
+//   const userId = uNum * 1111;
 
-  // Generate 2-4 submissions per user
-  const numberOfTestSongs = Math.floor(Math.random() * 3 + 2);
-  for (let i = 0; i < numberOfTestSongs; i++) {
-    // proper implementation could return some kind of result to
-    // pass on to the user, boolean here just for simplicity/testing
-    queueManager.trySubmission(
-      userId,
-      `https://TotallyAMusic.com/music/${userId}/${submissionNumber}`,
-      Math.floor(Math.random() * 121 + 90)
-    );
-    submissionNumber += 1;
-  }
-}
+//   // Generate 2-4 submissions per user
+//   const numberOfTestSongs = Math.floor(Math.random() * 3 + 2);
+//   for (let i = 0; i < numberOfTestSongs; i++) {
+//     // proper implementation could return some kind of result to
+//     // pass on to the user, boolean here just for simplicity/testing
+//     queueManager.trySubmission(
+//       userId,
+//       `https://TotallyAMusic.com/music/${userId}/${submissionNumber}`,
+//       Math.floor(Math.random() * 121 + 90)
+//     );
+//     submissionNumber += 1;
+//   }
+// }
 
-// For debug, print out all the users currently in the queue and
-// all their submissions.
-logger.info("Printing master queue + local queues");
-for (const sub of queueManager.masterQueue) {
-  logger.info(sub.userId);
-  for (const music of sub.localQueue) {
-    logger.info("    " + music.link);
-  }
-}
-logger.info("End of master queue");
+// // For debug, print out all the users currently in the queue and
+// // all their submissions.
+// logger.info("Printing master queue + local queues");
+// for (const sub of queueManager.masterQueue) {
+//   logger.info(sub.userId);
+//   for (const music of sub.localQueue) {
+//     logger.info("    " + music.link);
+//   }
+// }
+// logger.info("End of master queue");
 
-let next: Submission | null = null;
-// Loop through submission queue until no submissions left.
-let countdownToNewbieJoining = 5;
-while (queueManager.pollNextSubmission() !== null) {
-  next = queueManager.pollNextSubmission();
-  logger.info(
-    `Playing submission: Link: ${next?.link ?? "null"} - Time: ${(
-      (next?.secondsLong ?? 0) / 60
-    ).toFixed(2)}:${((next?.secondsLong ?? 0) % 60).toFixed(2)}`
-  );
-  countdownToNewbieJoining -= 1;
-  if (countdownToNewbieJoining === 0) {
-    logger.info("Adding 'late join' user + submissions");
-    const numberOfTestSongs = Math.floor(Math.random() * 3 + 2);
-    const newbieUserId = 9999;
-    for (let i = 0; i < numberOfTestSongs; i++) {
-      // proper implementation could return some kind of result with details
-      // to pass on to the user, boolean here just for simplicity/testing
-      queueManager.trySubmission(
-        newbieUserId,
-        `https://TotallyAMusic.com/music/${newbieUserId}/${submissionNumber}`,
-        Math.floor(Math.random() * 121 + 90)
-      );
-      submissionNumber += 1;
-    }
-  }
-}
+// let next: Submission | null = null;
+// // Loop through submission queue until no submissions left.
+// let countdownToNewbieJoining = 5;
+// while (queueManager.pollNextSubmission() !== null) {
+//   next = queueManager.pollNextSubmission();
+//   logger.info(
+//     `Playing submission: Link: ${next?.link ?? "null"} - Time: ${(
+//       (next?.secondsLong ?? 0) / 60
+//     ).toFixed(2)}:${((next?.secondsLong ?? 0) % 60).toFixed(2)}`
+//   );
+//   countdownToNewbieJoining -= 1;
+//   if (countdownToNewbieJoining === 0) {
+//     logger.info("Adding 'late join' user + submissions");
+//     const numberOfTestSongs = Math.floor(Math.random() * 3 + 2);
+//     const newbieUserId = 9999;
+//     for (let i = 0; i < numberOfTestSongs; i++) {
+//       // proper implementation could return some kind of result with details
+//       // to pass on to the user, boolean here just for simplicity/testing
+//       queueManager.trySubmission(
+//         newbieUserId,
+//         `https://TotallyAMusic.com/music/${newbieUserId}/${submissionNumber}`,
+//         Math.floor(Math.random() * 121 + 90)
+//       );
+//       submissionNumber += 1;
+//     }
+//   }
+// }
 ////////////// End Testing //////////////
