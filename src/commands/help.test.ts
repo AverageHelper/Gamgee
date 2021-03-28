43dfd520f58da8efac99e118bebc0619b610ea3f
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-03-28 14:46:03 -0600

    Start adding unit tests for commands

---
+++ src/commands/help.test.ts
@@ -0,0 +1,23 @@
+jest.mock("../actions/messages/replyToMessage");
+import * as replyToMessage from "../actions/messages/replyToMessage";
+import { CommandContext } from "./Command";
+const mockReplyPrivately = replyToMessage.replyPrivately as jest.Mock;
+
+import help from "./help";
+
+describe("Help command", () => {
+  test("describes all commands", async () => {
+    const context = ({
+      storage: null,
+      message: "Reply to this"
+    } as unknown) as CommandContext;
+
+    await help.execute(context);
+    expect(mockReplyPrivately).toHaveBeenCalledTimes(1);
+    expect(mockReplyPrivately).toHaveBeenCalledWith("Reply to this", expect.toBeString());
+
+    const calls = mockReplyPrivately.mock.calls[0] as Array<unknown>;
+    const description = calls[1];
+    expect(description).toMatchSnapshot();
+  });
+});
