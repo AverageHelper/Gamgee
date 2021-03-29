d0587ad479c5d1b0fda5a0bc52533e7e578ba587
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-03-28 18:13:11 -0600

    Scrape Bandcamp webpages' metadata for song information

---
+++ src/actions/getVideoDetails.test.ts
@@ -0,0 +1,80 @@
+import getVideoDetails from "./getVideoDetails";
+import { useLogger } from "../logger";
+
+describe("Video details", () => {
+  // YouTube
+  const testVid1 = "https://youtube.com/watch?v=9Y8ZGLiqXB8";
+
+  test.each`
+    desc                   | url
+    ${"no input"}          | ${""}
+    ${"invalid URL"}       | ${"not at all"}
+    ${"unavailable video"} | ${"https://youtu.be/9Y8ZGLiqXba"}
+    ${"is too short"}      | ${"https://www.youtube.com/watch?v=9Y8ZGL"}
+  `("returns null with $desc", async ({ url }: { url: string }) => {
+    const details = await getVideoDetails([url], null);
+    expect(details).toBeNull();
+  });
+
+  test.each`
+    desc                         | params                                                                                                | result
+    ${"is already good"}         | ${["https://youtube.com/watch?v=9RAQsdTQIcs"]}                                                        | ${"https://youtube.com/watch?v=9RAQsdTQIcs"}
+    ${"is shortened"}            | ${["https://youtu.be/9Y8ZGLiqXB8"]}                                                                   | ${testVid1}
+    ${"has extra info"}          | ${["https://youtu.be/9Y8ZGLiqXB8", "Text", "and", "stuff"]}                                           | ${testVid1}
+    ${"spams repeat characters"} | ${["https://youtu.be/9Y8ZGLiqXB8!!!!!!!!!!!!!!!!!!!!!!!!!!!!"]}                                       | ${testVid1}
+    ${"spams random characters"} | ${["https://youtu.be/9Y8ZGLiqXB8kdasu997ru53"]}                                                       | ${testVid1}
+    ${"is a playlist"}           | ${["https://www.youtube.com/watch?v=2rzoPFLRhqE&list=RDMM&start_radio=1&ab_channel=LucaStricagnoli"]} | ${"https://youtube.com/watch?v=2rzoPFLRhqE"}
+    ${"has channel info"}        | ${["https://www.youtube.com/watch?v=nY1WVAoMnYc&ab_channel=JonathanYoung"]}                           | ${"https://youtube.com/watch?v=nY1WVAoMnYc"}
+    ${"has time codes"}          | ${["https://www.youtube.com/watch?v=NFw-FrYmAEw&t=10s"]}                                              | ${"https://youtube.com/watch?v=NFw-FrYmAEw"}
+  `(
+    "returns info for a YouTube link that $desc",
+    async ({ params, result }: { params: Array<string>; result: string }) => {
+      const details = await getVideoDetails(params, null);
+      expect(details?.url).toBe(result);
+      expect(details?.duration.seconds).toBeDefined();
+      expect(details?.duration.seconds).toBeNumber();
+      expect(details?.duration.seconds).toBeGreaterThan(20);
+    }
+  );
+
+  // SoundCloud
+  const testVid2 = "https://soundcloud.com/hwps/no999";
+
+  test.each`
+    desc                | params                                                           | result
+    ${"is valid"}       | ${["https://soundcloud.com/hwps/no999"]}                         | ${testVid2}
+    ${"has extra info"} | ${["https://soundcloud.com/hwps/no999", "Text", "and", "stuff"]} | ${testVid2}
+  `(
+    "returns info for a SoundCloud link that $desc",
+    async ({ params, result }: { params: Array<string>; result: string }) => {
+      const details = await getVideoDetails(params, null);
+      expect(details?.url).toBe(result);
+      expect(details?.duration.seconds).toBeDefined();
+      expect(details?.duration.seconds).toBeNumber();
+      expect(details?.duration.seconds).toBeGreaterThan(20);
+    }
+  );
+
+  // BandCamp
+
+  test("returns null for bandcamp album links", async () => {
+    const url = "https://poniesatdawn.bandcamp.com/album/memories";
+    const details = await getVideoDetails([url], useLogger("debug"));
+    expect(details).toBe(null);
+  });
+
+  test("returns info for valid Bandcamp links", async () => {
+    const urls = [
+      "https://poniesatdawn.bandcamp.com/track/let-the-magic-fill-your-soul",
+      "https://forestrainmedia.com/track/bad-wolf",
+      "https://lehtmojoe.bandcamp.com/track/were-not-going-home-dallas-stars-2020"
+    ];
+    for (const url of urls) {
+      const details = await getVideoDetails([url], useLogger("debug"));
+      expect(details?.url).toBe(url);
+      expect(details?.duration.seconds).toBeDefined();
+      expect(details?.duration.seconds).toBeNumber();
+      expect(details?.duration.seconds).toBeGreaterThan(20);
+    }
+  });
+});
--- src/actions/getVideoDetails.ts
+++ src/actions/getVideoDetails.ts
@@ -1,10 +1,9 @@
 import ytdl from "ytdl-core";
 import yts from "yt-search";
 import SoundCloud from "soundcloud-scraper";
+import urlMetadata from "url-metadata";
 import richErrorMessage from "../helpers/richErrorMessage";
-import { useLogger } from "../logger";
-
-const logger = useLogger();
+import { Logger, useLogger } from "../logger";
 
 interface VideoDetails {
   url: string;
@@ -29,22 +28,32 @@ interface VideoDetails {
  *
  * @returns A set of video details, or `null` if no video could be found from the provided query.
  */
-export default async function getVideoDetails(args: Array<string>): Promise<VideoDetails | null> {
+export default async function getVideoDetails(
+  args: Array<string>,
+  logger: Logger | null = useLogger()
+): Promise<VideoDetails | null> {
   // Try the first value as a video URL
   const urlString = args[0];
   if (urlString === undefined || urlString === "") return null;
 
   // Try YouTube URL
   if (ytdl.validateURL(urlString)) {
-    const videoId = ytdl.getURLVideoID(urlString);
-    logger.info(`Got video ID '${videoId}'`);
-    const result = await yts({ videoId });
-    return {
-      fromUrl: true,
-      url: result.url,
-      title: result.title,
-      duration: result.duration
-    };
+    try {
+      const videoId = ytdl.getURLVideoID(urlString);
+      logger?.info(`Got video ID '${videoId}'`);
+      const result = await yts({ videoId });
+      return {
+        fromUrl: true,
+        url: result.url,
+        title: result.title,
+        duration: result.duration
+      };
+    } catch (error: unknown) {
+      logger?.error(
+        richErrorMessage(`Failed to fetch song from YouTube using url '${urlString}'`, error)
+      );
+      return null;
+    }
   }
 
   // Try SoundCloud
@@ -60,8 +69,43 @@ export default async function getVideoDetails(args: Array<string>): Promise<Vide
 
     // Something went wrong. Is this a valid SoundCloud link?
   } catch (error: unknown) {
-    logger.error(
-      richErrorMessage(`Failed to fetch song from SoundCloud using url '${urlString}'`, error)
+    if (error instanceof TypeError) {
+      logger?.debug(
+        `Failed to fetch song from SoundCloud using url '${urlString}'. Likely not a SoundCloud link`
+      );
+    } else {
+      logger?.error(
+        richErrorMessage(`Failed to fetch song from SoundCloud using url '${urlString}'`, error)
+      );
+      return null;
+    }
+  }
+
+  // Try Bandcamp
+  try {
+    // Get the webpage
+    const metadata = await urlMetadata(urlString);
+
+    const json = metadata.jsonld as
+      | { name?: string; additionalProperty?: Array<{ name: string; value: number }> }
+      | undefined;
+    const seconds: number | null =
+      json?.additionalProperty?.find(property => property.name === "duration_secs")?.value ?? null;
+    const title: string | null = json?.name ?? null;
+
+    if (seconds === null || title === null) return null;
+
+    return {
+      fromUrl: true,
+      url: metadata.url,
+      title: metadata.title,
+      duration: { seconds }
+    };
+
+    // Something went wrong. Is this a valid Bandcamp link?
+  } catch (error: unknown) {
+    logger?.error(
+      richErrorMessage(`Failed to fetch song from Bandcamp using url '${urlString}'`, error)
     );
     return null;
   }
