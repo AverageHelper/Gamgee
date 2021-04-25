fdeb12a6909bff66f75dac65ae7b4ac65c16524d
James Robinson <1500092+AverageHelper@users.noreply.github.com>
2021-04-25 01:19:13 -0600

    Logging improvements (#30)
    * Don't bother with logging timestamps to console now that I know how to make PM2 do it
    
    * Don't log in our common mention parsers

--- src/helpers/getChannelIdFromMention.ts
+++ src/helpers/getChannelIdFromMention.ts
@@ -1,14 +1,11 @@
-import type { Logger } from "../logger";
-
 /**
  * Get a channel ID from a mention string.
  *
  * @param mention The mention string, in the form `<#[0-9]>`.
- * @param logger The logger to which to send debug messages.
  *
  * @returns The ID string between the mention markers.
  */
-export default function getChannelIdFromMention(mention: string, logger: Logger): string | null {
+export default function getChannelIdFromMention(mention: string): string | null {
   let id = mention.slice();
   if (!id) return null;
 
@@ -18,10 +15,8 @@ export default function getChannelIdFromMention(mention: string, logger: Logger)
   if (startsRight && endsRight) {
     id = id.slice(2, -1);
 
-    logger.debug(`Channel ${id} was mentioned.`);
     return id;
   }
 
-  logger.debug("No channel was mentioned.");
   return null;
 }
--- src/helpers/getUserIdFromMention.ts
+++ src/helpers/getUserIdFromMention.ts
@@ -1,15 +1,12 @@
-import type { Logger } from "../logger";
-
 /**
  * Get a user ID from a mention string.
  * @see https://discordjs.guide/miscellaneous/parsing-mention-arguments.html#implementation
  *
  * @param mention The mention string, in the form `<@!?[0-9]>`.
- * @param logger The logger to which to send debug messages.
  *
  * @returns The ID string between the mention markers.
  */
-export default function getUserIdFromMention(mention: string, logger: Logger): string | null {
+export default function getUserIdFromMention(mention: string): string | null {
   let id = mention.slice();
   if (!id) return null;
 
@@ -23,10 +20,8 @@ export default function getUserIdFromMention(mention: string, logger: Logger): s
       id = id.slice(1);
     }
 
-    logger.debug(`User ${id} was mentioned.`);
     return id;
   }
 
-  logger.debug("No user was mentioned.");
   return null;
 }
