3f121b230463f74fe05d07b88175401f640e09bd
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-04-28 14:48:06 -0600

    Add info about the `now-playing` to the `sr info` command

--- src/commands/songRequest/info.ts
+++ src/commands/songRequest/info.ts
@@ -8,6 +8,7 @@ const info: NamedSubcommand = {
   description: "Print a handy message to let people know how to queue-up.",
   async execute({ message, storage }) {
     const songRequest = (await import("./index")).default;
+    const nowPlaying = (await import("../nowPlaying")).default;
 
     // Print the standard help
     const COMMAND_PREFIX = await getConfigCommandPrefix(storage);
@@ -21,6 +22,11 @@ const info: NamedSubcommand = {
     helpBuilder.pushNewLine();
     helpBuilder.push(
       "I will respond with a text verification indicating your song has joined the queue!"
+    );
+    helpBuilder.pushNewLine();
+    helpBuilder.pushNewLine();
+    helpBuilder.push(
+      `To see the current song, type \`${COMMAND_PREFIX}${nowPlaying.name}\` and check your DMs.`
     );
 
     return reply(message, helpBuilder.result());
