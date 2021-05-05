a07d352e2c36c82c179e4481ec0efaeec8dd46a2
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-05-04 20:39:57 -0600

    Don't allow users to run `?now-playing` in DMs

--- src/commands/nowPlaying.test.ts
+++ src/commands/nowPlaying.test.ts
@@ -2,6 +2,7 @@ jest.mock("../actions/messages");
 jest.mock("../actions/queue/useQueue");
 jest.mock("../actions/queue/getQueueChannel");
 jest.mock("../permissions");
+jest.mock("./songRequest/actions");
 
 import * as messageActions from "../actions/messages";
 const mockReplyWithMention = messageActions.replyWithMention as jest.Mock;
@@ -17,8 +18,12 @@ const mockGetQueueChannel = getQueueChannel as jest.Mock;
 import { userIsAdminForQueueInGuild } from "../permissions";
 const mockUserIsAdminForQueueInGuild = userIsAdminForQueueInGuild as jest.Mock;
 
+import { reply } from "./songRequest/actions";
+const mockReply = reply as jest.Mock;
+
 const mockGetAllEntries = jest.fn();
 
+import type Discord from "discord.js";
 import nowPlaying from "./nowPlaying";
 import { useTestLogger } from "../../tests/testUtils/logger";
 import type { CommandContext } from "./Command";
@@ -28,17 +33,19 @@ const logger = useTestLogger("error");
 
 describe("Now-Playing", () => {
   const queueChannelId = "queue-channel";
-  const context = {
-    logger,
-    message: {
-      guild: "the guild",
-      channel: {
-        id: "not-queue-channel"
-      }
-    }
-  };
+  let context: CommandContext;
 
   beforeEach(() => {
+    context = ({
+      logger,
+      message: {
+        guild: "the guild",
+        channel: {
+          id: "not-queue-channel"
+        }
+      }
+    } as unknown) as CommandContext;
+
     mockUseQueue.mockReturnValue({
       getAllEntries: mockGetAllEntries
     });
@@ -50,12 +57,22 @@ describe("Now-Playing", () => {
     mockGetQueueChannel.mockResolvedValue({ id: queueChannelId });
   });
 
+  test("does nothing when not in a guild", async () => {
+    context.message = ({
+      guild: null
+    } as unknown) as Discord.Message;
+    await expect(nowPlaying.execute(context)).resolves.toBe(undefined);
+
+    expect(mockDeleteMessage).not.toHaveBeenCalled();
+    expect(mockReplyPrivately).not.toHaveBeenCalled();
+    expect(mockReply).toHaveBeenCalledTimes(1);
+    expect(mockReply).toHaveBeenCalledWith(context.message, "Can't do that here.");
+  });
+
   test("informs the user when no queue is set up", async () => {
     mockGetQueueChannel.mockResolvedValue(null);
 
-    await expect(nowPlaying.execute((context as unknown) as CommandContext)).resolves.toBe(
-      undefined
-    );
+    await expect(nowPlaying.execute(context)).resolves.toBe(undefined);
 
     expect(mockUseQueue).not.toHaveBeenCalled();
 
@@ -79,9 +96,7 @@ describe("Now-Playing", () => {
     async ({ values }: { values: Array<QueueEntry> }) => {
       mockGetAllEntries.mockResolvedValue(values);
 
-      await expect(nowPlaying.execute((context as unknown) as CommandContext)).resolves.toBe(
-        undefined
-      );
+      await expect(nowPlaying.execute(context)).resolves.toBe(undefined);
 
       expect(mockUseQueue).toHaveBeenCalledTimes(1);
 
@@ -108,9 +123,7 @@ describe("Now-Playing", () => {
     async ({ values }: { values: Array<QueueEntry> }) => {
       mockGetAllEntries.mockResolvedValue(values);
 
-      await expect(nowPlaying.execute((context as unknown) as CommandContext)).resolves.toBe(
-        undefined
-      );
+      await expect(nowPlaying.execute(context)).resolves.toBe(undefined);
 
       expect(mockUseQueue).toHaveBeenCalledTimes(1);
 
