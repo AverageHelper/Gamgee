8538c76d8ec008331ebc899ec5652c3e83906c4c
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-08-20 23:25:35 -0600

    Fix the version command's random celebration

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -6,8 +6,13 @@ const version: Command = {
 	name: "version",
 	description: "Display the bot's current codebase version.",
 	requiresGuild: false,
-	async execute({ reply }) {
-		return reply(`I'm currently running Gamgee Core v${gamgeeVersion}.  ${randomCelebration()}`);
+	async execute({ reply, user }) {
+		const celebration = randomCelebration().unwrapFirstWith({
+			me: "Me",
+			otherUser: user,
+			otherMember: null
+		});
+		return reply(`I'm currently running Gamgee Core v${gamgeeVersion}.  ${celebration}`);
 	}
 };
 
