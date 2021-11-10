efe6ea2a2d2f8b4382c2bb8c81bcf5372647e748
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-11-10 00:03:16 -0700

    Fix unit tests, probably

--- src/helpers/startsWithVowel.ts
+++ src/helpers/startsWithVowel.ts
@@ -2,6 +2,7 @@
  * Returns `true` if the provided string starts with an English vowel.
  */
 export function startsWithVowel(str: string): boolean {
+	if (typeof str !== "string") return false;
 	const first = str[0];
 	if (first === undefined) return false;
 	return ["a", "e", "i", "o", "u"].includes(first.toLowerCase());
