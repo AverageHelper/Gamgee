baab3b8c31b5d9e80ae3e0b13f02e9c29b926efc
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2022-08-17 12:53:53 -0600

    `?userinfo` now works as a message command, thanks to new CommandContext interface

--- src/commands/userinfo.ts
+++ src/commands/userinfo.ts
@@ -1,5 +1,12 @@
 import type { Command } from "./Command.js";
-import { localizations } from "../i18n.js";
+import { DEFAULT_LOCALE, isSupportedLocale, localizations } from "../i18n.js";
+import {
+	composed,
+	createPartialString,
+	push,
+	pushCode,
+	pushSpace
+} from "../helpers/composeStrings.js";
 
 // TODO: i18n
 export const userinfo: Command = {
@@ -8,23 +15,36 @@ export const userinfo: Command = {
 	description: "Returns some information about you.",
 	descriptionLocalizations: localizations("commands.userinfo.description"),
 	requiresGuild: false,
-	async execute(context) {
-		if (context.type !== "interaction") {
-			await context.deleteInvocation();
-			return await context.replyPrivately(`Try \`/${userinfo.name}\` instead!`);
-		}
-		const { interaction, replyPrivately } = context;
-
-		const guildLocale = interaction.guildLocale;
-		const locale = interaction.locale;
-
+	async execute({ guildLocaleRaw, userLocaleRaw, deleteInvocation, replyPrivately }) {
 		// TODO: Create an embed with some user details
-		if (guildLocale) {
-			await replyPrivately(
-				`[WIP] Your locale is \`${locale}\`, and your guild's locale is \`${guildLocale}\``
-			);
+
+		await deleteInvocation();
+
+		const response = createPartialString("Your locale is ");
+		pushCode(userLocaleRaw ?? "unknown", response);
+		pushSpace(response);
+		if (userLocaleRaw) {
+			if (isSupportedLocale(userLocaleRaw)) {
+				push("(supported)", response);
+			} else {
+				push(
+					isSupportedLocale(userLocaleRaw) ? "(supported)" : `(not supported, so we'll go with `,
+					response
+				);
+				pushCode(DEFAULT_LOCALE, response);
+				push(")", response);
+			}
 		} else {
-			await replyPrivately(`[WIP] Your locale is \`${locale}\``);
+			push(`(so we'll assume `, response);
+			pushCode(DEFAULT_LOCALE, response);
+			push(")", response);
 		}
+
+		if (guildLocaleRaw) {
+			push(", and your guild's locale is ", response);
+			pushCode(guildLocaleRaw, response);
+		}
+
+		await replyPrivately(composed(response));
 	}
 };
