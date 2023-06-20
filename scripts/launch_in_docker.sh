11a5e49739fc75da12d950d7e24b6674406375ca
Karcsesz <git@karcsesz.hu>
2023-06-20 11:53:37 +0200

    Prevent paths with spaces in them exploding

--- scripts/launch_in_docker.sh
+++ scripts/launch_in_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DATABASE_FILENAME=$(echo $DATABASE_URL | sed "s/file://g")
+DATABASE_FILENAME="$(echo $DATABASE_URL | sed "s/file://g")"
 if [[ ! -f $DATABASE_FILENAME ]] ; then
   echo "No database detected, starting firstrun..."
   npm run firstrun:runtime
