bf954ed0887eeca50d3b8f4607cc68ed6b770948
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2023-07-03 19:43:01 -0600

    chore: Fix `NonEmptyArray` global-ness

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,18 +1,20 @@
 import "jest-extended";
 
-/**
- * An array that always contains at least one element.
- */
-type NonEmptyArray<T> = [T, ...Array<T>];
+declare global {
+	/**
+	 * An array that always contains at least one element.
+	 */
+	type NonEmptyArray<T> = [T, ...Array<T>];
 
-/**
- * A function which determines the type identity of the given value.
- */
-type TypeGuard<T> = (tbd: unknown) => tbd is T;
+	/**
+	 * A function which determines the type identity of the given value.
+	 */
+	type TypeGuard<T> = (tbd: unknown) => tbd is T;
 
-/**
- * The element type of the given array.
- */
-type GetArrayElementType<T extends ReadonlyArray<unknown>> = T extends ReadonlyArray<infer U>
-	? U
-	: never;
+	/**
+	 * The element type of the given array.
+	 */
+	type GetArrayElementType<T extends ReadonlyArray<unknown>> = T extends ReadonlyArray<infer U>
+		? U
+		: never;
+}
