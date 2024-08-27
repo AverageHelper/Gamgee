5ac1aa777195aaa47594a74f5902b4e784869669
AverageHelper <me@average.name>
2024-08-27 12:46:45 -0600

    feat: Cache user locales (useful when one interaction leads to a private follow-up with another user, whose locale the interaction doesn't know)

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,9 +1,12 @@
+import { cacheLocaleFromInteraction } from "../i18nCache.js";
 import { handleButton } from "../handleButton.js";
 import { handleInteraction } from "../handleInteraction.js";
 import { onEvent } from "../helpers/onEvent.js";
 
 export const interactionCreate = onEvent("interactionCreate", {
 	async execute(interaction, logger) {
+		cacheLocaleFromInteraction(interaction);
+
 		if (interaction.isChatInputCommand()) {
 			await handleInteraction(interaction, logger);
 		} else if (interaction.isButton()) {
