18e22580f45a59343efc62213c20c5c779d557fa
AverageHelper <me@average.name>
2026-01-20 11:53:27 -0700

    fix(docker/db): run pending migrations against the database on startup

--- scripts/launch_in_docker.sh
+++ scripts/launch_in_docker.sh
@@ -3,7 +3,10 @@ set -euo pipefail
 
 DATABASE_FILENAME="$(echo $DATABASE_URL | sed "s/file://g")"
 if [[ ! -f $DATABASE_FILENAME ]] ; then
-  echo "No database detected, starting firstrun..."
+  echo 'No database detected, starting firstrun...'
   npm run firstrun:runtime
+else
+  echo 'Applying migrations...'
+  npm run migrate
+  node .
 fi
-node .
